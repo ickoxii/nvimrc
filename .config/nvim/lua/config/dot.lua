004c3ff4e1956acee33daf7ce5b44c3b6c2ce60e
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-10-07 22:37:26 -0500

    graphviz compilation

---
+++ .config/nvim/lua/config/dot.lua
@@ -0,0 +1,20 @@
+-- Auto-compile .dot files to PDF on save
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = "*.dot",
+  callback = function(args)
+    local filepath = args.file
+    local pdfpath = filepath:gsub("%.dot$", ".pdf")
+    vim.fn.jobstart({ "dot", "-Tpdf", filepath, "-o", pdfpath }, {
+      on_exit = function(_, code, _)
+        -- if code == 0 then
+        --   vim.notify("Compiled " .. filepath .. " → " .. pdfpath, vim.log.levels.INFO)
+        -- else
+        --   vim.notify("Error compiling " .. filepath, vim.log.levels.ERROR)
+        -- end
+        if code ~= 0 then
+          vim.notify("Error compiling " .. filepath, vim.log.levels.ERROR)
+        end
+      end,
+    })
+  end,
+})
