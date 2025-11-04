e99ddc5b4838acc14f48fa28dfdc736f80f7fa45
Icko Iben <80863308+ickoxii@users.noreply.github.com>
2025-11-03 19:59:27 -0600

    simplify colors, replace poon remaps

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,46 +1,10 @@
 -- Harpoon
--- https://github.com/ThePrimeagen/harpoon
-
--- return {
---   {
---     "ThePrimeagen/harpoon",
---     lazy = false,
---     config = function()
---       local mark = require("harpoon.mark")
---       local ui = require("harpoon.ui")
---
---       vim.keymap.set("n", "<leader>a", mark.add_file)
---       vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
---
---       vim.keymap.set("n", "<C-h>", function()
---         ui.nav_file(1)
---       end)
---       vim.keymap.set("n", "<C-j>", function()
---         ui.nav_file(2)
---       end)
---       vim.keymap.set("n", "<C-k>", function()
---         ui.nav_file(3)
---       end)
---       vim.keymap.set("n", "<C-l>", function()
---         ui.nav_file(4)
---       end)
---     end,
---   },
--- }
-
--- Harpoon
 -- https://github.com/ThePrimeagen/harpoon/tree/harpoon2
---
--- `Error while dumping encode_tv2json() argument`:
--- https://github.com/ThePrimeagen/harpoon/issues/390
---
--- General issues with harpoon managing sessions
 
 return {
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    -- commit = "e76cb03",
     dependencies = { "nvim-lua/plenary.nvim" },
     lazy = false,
     config = function()
@@ -70,6 +34,19 @@ return {
       vim.keymap.set("n", "<C-l>", function()
         harpoon:list():select(4)
       end, { desc = "Harpoon: Switch to poon 4" })
+
+      vim.keymap.set("n", "<leader><C-h>", function()
+        harpoon:list():replace_at(1)
+      end)
+      vim.keymap.set("n", "<leader><C-j>", function()
+        harpoon:list():replace_at(2)
+      end)
+      vim.keymap.set("n", "<leader><C-k>", function()
+        harpoon:list():replace_at(3)
+      end)
+      vim.keymap.set("n", "<leader><C-l>", function()
+        harpoon:list():replace_at(4)
+      end)
     end,
   },
 }
